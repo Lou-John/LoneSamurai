20374df0dca70a5736ff8868ee00a3cf55d307e8
aleski.menard <aleski.menard@gmail.com>
2024-01-25 15:10:45 +0100

    add 3 levels

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -6,10 +6,22 @@ public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
+    private const string SoundVolumeKey = "SoundVolume";
 
-    public void setMusicVolume()
+    
+    private void Start()
     {
-        float volume = musicSlider.value;
-        myMixer.SetFloat("Music", Mathf.Log10(volume)*20);
+        float savedSoundVolume = PlayerPrefs.GetFloat(SoundVolumeKey, 1.0f);
+        myMixer.SetFloat("Music", savedSoundVolume);
+        musicSlider.value = savedSoundVolume;
+        musicSlider.onValueChanged.AddListener(OnSoundVolumeChanged);
+    }
+
+    void OnSoundVolumeChanged(float newVolume)
+    {
+        // Set the sound volume and save it to PlayerPrefs
+        myMixer.SetFloat("Music", Mathf.Log10(newVolume) * 20);
+        PlayerPrefs.SetFloat(SoundVolumeKey, newVolume);
+        PlayerPrefs.Save(); // Ensure changes are saved immediately
     }
 }
